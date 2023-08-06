ed8c30a31140196a5c80dc05ba1d19a1b02026cc
JasonPaulino <jasonp0830@gmail.com>
2023-08-06 18:06:58 -0400

    finished next-1, working on next02

--- next05/lib/getFormattedDate.tsx
+++
@@ -1,8 +0,0 @@
-export default function getFormattedDate(dateString: string): string {
-    return new Date(dateString).toLocaleString('en-US', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
-    });
-  }
-  
